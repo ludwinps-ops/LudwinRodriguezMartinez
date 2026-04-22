ea82588f37668d9848d366806e33d679a1fec943
ludwinps-ops <ludwinps@gmail.com>
2026-04-22 17:56:37 -0500

    asd

--- js/tarea3JS.js
+++ js/tarea3JS.js
@@ -18,3 +18,5 @@ function notoqs(){
 function  murky(){
     document.getElementById("hots").src="https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExMzl3a3k4NTFiYzI5Y2E3NGxoaXNhdndmanJhc2s1M3k2aW43d3c2aCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/g04CfRZogHyxJEPmtO/giphy.gif";
 }
+
+function
