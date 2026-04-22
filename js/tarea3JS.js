ec29be6804a03a2f4279d327f8a79d6147263b6c
ludwinps-ops <ludwinps@gmail.com>
2026-04-22 17:48:13 -0500

    asd

--- js/tarea3JS.js
+++ js/tarea3JS.js
@@ -8,9 +8,13 @@ function eje9(){
     let adicional = 3
     let tarifa = 10;
     let total = (distancia*adicional)+tarifa;
-    alert("Recorriste "+distancia+"Km su total a pagar es: S/."+total);
+    alert("Recorriste "+distancia+" Km su total a pagar es: S/."+total);
 }
 
 function notoqs(){
     alert("Te dije que no toques.");
+}
+
+function  murky(){
+    document.getElementById("hots").src="https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExMzl3a3k4NTFiYzI5Y2E3NGxoaXNhdndmanJhc2s1M3k2aW43d3c2aCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/g04CfRZogHyxJEPmtO/giphy.gif";
 }
