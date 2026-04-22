a292291faa3e905d5d67671a5714ee8bb77632f1
ludwinps-ops <ludwinps@gmail.com>
2026-04-22 16:59:04 -0500

    a

--- js/tarea3JS.js
+++ js/tarea3JS.js
@@ -3,9 +3,11 @@ console.log("Entrando a las Tareas de JS");
 //alert nos sirve para mostrarle un alert al usuario
 alert("Estas entrando a las tareas de JS");
 
-function ejercicio9(){
+function ejercicio09(){
     let num1 = 3;
     let tarifa = 10;
     let operación = (num1*3)+tarifa;
     alert("El total a pagar es"+operación);
 }
+
+function
