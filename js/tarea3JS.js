14811834f5e23daa273c32a574d857f055f0b9b8
ludwinps-ops <ludwinps@gmail.com>
2026-04-22 16:56:18 -0500

    e9

---
+++ js/tarea3JS.js
@@ -0,0 +1,11 @@
+// Console.log nos sirve para depurar y probar nuestras salidas
+console.log("Entrando a las Tareas de JS");
+//alert nos sirve para mostrarle un alert al usuario
+alert("Estas entrando a las tareas de JS");
+
+function ejercicio9(){
+    let num1 = 3;
+    let tarifa = 10;
+    let operación = (num1*3)+tarifa;
+    alert("El total a pagar es"+operación);
+}
