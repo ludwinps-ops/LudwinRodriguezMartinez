44454dedf40c1b51f02ebc40443c1b4835c42a5f
ludwinps-ops <ludwinps@gmail.com>
2026-04-22 16:36:01 -0500

    pagina 4

--- js/primerJS.js
+++ js/primerJS.js
@@ -32,3 +32,41 @@ function nombresNav2(){
     document.getElementById("m7").textContent="Personal";
     document.getElementById("m8").textContent="Entregable";
 }
+function cambiarTitulo(){
+    document.getElementById("titulo").textContent="Practicando JS";
+    document.getElementById("titulo").style.color="#290c60" 
+    document.getElementById("logoSenati").src="images/senati3.png";
+    document.getElementById("encabezado").style.background="#89cce2"
+}
+
+function suma(){
+    let num1 = 3;
+    let num2 = 5;
+    let suma = num1+num2;
+    alert("la suma es de" +suma);
+}
+function eje01(){
+    let p1 = 10;
+    let p2 = 5;
+    let p3 = 15;
+    let c1 = 3;
+    let c2 = 18;
+    let c3 = 6;
+    let Stotal, total;
+    Stotal = (p1*c1)+(p2*c2)+(p3*c3);
+    if(Stotal>100){
+        total = Stotal*0.95;
+        alert("Su compra lleva descuento");
+    }else{
+        total = Stotal;
+    }
+    alert("El total a Pagar es de: S/."+total);
+
+}
+function eje02(){
+    let amigos = 10;
+    let total = 100;
+    let cuota = total/amigos;
+    alert("Son "+amigos+" amigos"+ " y la cuota del total S/."+total+" es de : S/."+cuota);
+}
+
