4b4cf0377dd79e74cc3a5d0190d4584163834cd3
ludwinps-ops <ludwinps@gmail.com>
2026-04-22 15:29:18 -0500

    asd

--- js/primerJS.js
+++ js/primerJS.js
@@ -5,6 +5,7 @@ alert("Hola Mundo. Esto es una Alerta");
 
 function cambiarCarita(){
     document.getElementById("carita").src="imagenes/triste.png";
+    document.getElementById("logoSenati").src="imagenes/logo.png"
     alert("Por que tas triste");
 }
 function feliz(){
